b4ca3584917413606f0c6d037ff5293eca1f6665
Jacob Shorty <jakeshorty@gmail.com>
2015-01-25 18:30:47 -0500

    Finish working version

--- word_chains.rb
+++ word_chains.rb
@@ -7,26 +7,20 @@ class WordChainer
 
   def run(source, target)
     @current_words = [source]
-    @all_seen_words = [source]
+    @all_seen_words = { source => nil }
 
     until @current_words.empty?
-      new_current_words = []
-
-      @current_words.each do |word|
-        adjacent_words(word).each do |new_word|
-          next if @all_seen_words.include?(new_word)
-          new_current_words << new_word
-          @all_seen_words << new_word
-        end
-      end
-
+      new_current_words = explore_current_words
       @current_words = new_current_words
-      puts "NEW CURRENT WORDS:"
-      puts @current_words
+      break if @current_words.include?(target)
+    end
+
+    if @all_seen_words.include?(target)
+      build_path(target)
+    else
+      puts "No path between the two words was found in this dictionary."
     end
   end
-
-
 
   def adjacent_words(word)
     same_length_words = @dictionary.select { |dict_word|
@@ -44,4 +38,26 @@ class WordChainer
     adjacent_words
   end
 
+  def explore_current_words
+    new_current_words = []
+    @current_words.each do |word|
+      adjacent_words(word).each do |new_word|
+        next if @all_seen_words.include?(new_word)
+        new_current_words << new_word
+        @all_seen_words[new_word] = word
+      end
+    end
+    #new_current_words.each do |new_word|
+    #  puts "#{@all_seen_words[new_word]} => #{new_word}"
+    #end
+    new_current_words
+  end
+
+  def build_path(target)
+    path = [target]
+    until path.last.nil?
+      path << @all_seen_words[path.last]
+    end
+    path.reverse.drop(1)
+  end
 end
