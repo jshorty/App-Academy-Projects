872c8c01897fc37912a1a99c0add17e14e827510
Jacob Shorty <jakeshorty@gmail.com>
2015-01-25 17:57:28 -0500

    Add breadth-first word chain builder

--- word_chains.rb
+++ word_chains.rb
@@ -1,7 +1,32 @@
+require 'set'
+
 class WordChainer
   def initialize(dictionary_file_name)
-    @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
+    @dictionary = File.readlines(dictionary_file_name).map(&:chomp).to_set
   end
+
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = [source]
+
+    until @current_words.empty?
+      new_current_words = []
+
+      @current_words.each do |word|
+        adjacent_words(word).each do |new_word|
+          next if @all_seen_words.include?(new_word)
+          new_current_words << new_word
+          @all_seen_words << new_word
+        end
+      end
+
+      @current_words = new_current_words
+      puts "NEW CURRENT WORDS:"
+      puts @current_words
+    end
+  end
+
+
 
   def adjacent_words(word)
     same_length_words = @dictionary.select { |dict_word|
@@ -18,4 +43,5 @@ class WordChainer
 
     adjacent_words
   end
+
 end
