6bf62f9281d19615389eae1b92bfc36b7112bfa9
Jacob Shorty <jakeshorty@gmail.com>
2015-01-29 10:02:10 -0500

    Create Board class, add Piece methods

--- piece.rb
+++ piece.rb
@@ -1,10 +1,25 @@
 class Piece
-  def initialize(color, king)
+  attr_reader :color
+  attr_accessor :position
+
+  def initialize(color, king, position, board)
     @color = color
     @king = false
+    @position = position
   end
 
-  def promote
-    @king = true
+  def move_diffs
+    [1, 1, -1, -1].permutation(2).to_a.uniq
+  end
+
+  def perform_slide(end_pos)
+    if position
+  end
+
+  def perform_jump(end_pos)
+  end
+
+  def maybe_promote
+    @king = true if position == #BACK ROW
   end
 end
