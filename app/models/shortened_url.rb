dcfee72e015452259d898f1f8ca5a7a6c4b106ba
Jacob Shorty <jakeshorty@gmail.com>
2015-02-04 13:36:36 -0500

    Add ShortUrl factory method with random code generation

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -3,11 +3,25 @@ class ShortenedUrl < ActiveRecord::Base
   validates :short_url, presence: true, uniqueness: true
   validates :submitter_id, presence: true
 
+  belongs_to :submitter,
+    class_name: "User",
+    foreign_key: :submitter_id,
+    primary_key: :id
+
   def self.random_code
     code = SecureRandom::urlsafe_base64(16)
 
-    while ShortenedUrl.where(short_url: code).empty?
+    while ShortenedUrl.where(short_url: code).any?
       code = SecureRandom::urlsafe_base64(16)
     end
+
+    code
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    short_url = ShortenedUrl.random_code
+    ShortenedUrl.create! long_url: long_url,
+                         submitter_id: user.id,
+                         short_url: short_url
   end
 end
