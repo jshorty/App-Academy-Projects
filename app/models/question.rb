e1eea9d15783f6c39ce620e20336204f7fd6e808
Jacob Shorty <jakeshorty@gmail.com>
2015-02-05 14:31:28 -0500

    Add results method.

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,4 +12,20 @@ class Question < ActiveRecord::Base
     primary_key: :id
 
   has_many :responses, through: :answer_choices, source: :responses
+
+  def results
+  choice_counts = self
+    .answer_choices
+    .select("answer_choices.*, COUNT(responses.*) AS responses_count")
+    .joins("LEFT OUTER JOIN responses ON answer_choices.id = responses.answer_choice_id")
+    .group("answer_choices.id")
+
+    choices_with_counts = {}
+    choice_counts.each do |answer_choice|
+      choices_with_counts[answer_choice.text] = answer_choice.responses_count
+    end
+
+    choices_with_counts
+  end
+
 end
