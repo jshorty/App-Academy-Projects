3dd5826a5ab48340fbc199ef6c80a498d2b1837f
Jacob Shorty <jakeshorty@gmail.com>
2015-02-09 14:23:19 -0500

    Created contact shares and associations

--- FirstRoutes/app/models/contact.rb
+++ FirstRoutes/app/models/contact.rb
@@ -1,6 +1,16 @@
 class Contact < ActiveRecord::Base
   validates :email, :user_id, :name, :presence => true
-  validates :email, :uniqueness => {:scope => :user_id} 
+  validates :email, :uniqueness => {:scope => :user_id}
 
+  belongs_to :owner,
+    class_name: "User",
+    primary_key: :id,
+    foreign_key: :user_id
 
+  has_many :contact_shares,
+    class_name: "ContactShare",
+    primary_key: :id,
+    foreign_key: :contact_id
+
+  has_many :shared_users, through: :contact_shares, source: :user
 end
