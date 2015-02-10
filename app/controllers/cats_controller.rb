fffa6a0a9463f7b788d3274c83ac6d84e02b7f0a
Jacob Shorty <jakeshorty@gmail.com>
2015-02-10 14:13:09 -0500

    Add edit and update actions

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -21,7 +21,20 @@ class CatsController < ApplicationController
     else
       render :new
     end
+  end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
   end
 
   private
