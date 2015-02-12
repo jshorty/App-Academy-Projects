d9daa7db1518389f9533bc55543552de55a237fb
Jacob Shorty <jakeshorty@gmail.com>
2015-02-12 15:11:57 -0500

    Create notes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212164341) do
+ActiveRecord::Schema.define(version: 20150212200847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20150212164341) do
 
   create_table "bands", force: true do |t|
     t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notes", force: true do |t|
+    t.text     "text",       null: false
+    t.integer  "user_id",    null: false
+    t.integer  "track_id",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
