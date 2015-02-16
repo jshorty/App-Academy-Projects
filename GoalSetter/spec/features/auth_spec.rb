abccd43a69d205d930195b442410580ae745c4be
Jacob Shorty <jakeshorty@gmail.com>
2015-02-16 10:40:27 -0500

    Add signup/login tests

--- GoalSetter/spec/features/auth_spec.rb
+++ GoalSetter/spec/features/auth_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rails_helper'
 
 feature "the signup process" do
 
@@ -9,22 +10,34 @@ feature "the signup process" do
 
   feature "signing up a user" do
 
-    scenario "shows username on the homepage after signup"
+    scenario "shows username on the homepage after signup" do
+      sign_up("kitty_cat", "password")
+      expect(page).to have_content "kitty_cat"
+      expect(page).to have_content "Goals"
+    end
+  end
+end
 
+feature "logging in" do
+
+  scenario "has a log-in page" do
+    visit new_session_url
+    expect(page).to have_content "Log In"
+  end
+
+  scenario "shows username on the homepage after login" do
+    sign_up("kitty_cat", "password")
+    log_in("kitty_cat", "password")
+    expect(page).to have_content "kitty_cat"
+    expect(page).to have_content "Goals"
   end
 
 end
 
-# feature "logging in" do
-#
-#   scenario "shows username on the homepage after login"
-#
-# end
-#
-# feature "logging out" do
-#
-#   scenario "begins with logged out state"
-#
-#   scenario "doesn't show username on the homepage after logout"
-#
-# end
+feature "logging out" do
+
+  scenario "begins with logged out state"
+
+  scenario "doesn't show username on the homepage after logout"
+
+end
