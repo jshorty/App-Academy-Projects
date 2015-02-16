af5b9c92a7213bdb49667d8471a235934713d260
Jacob Shorty <jakeshorty@gmail.com>
2015-02-16 10:55:37 -0500

    add user model and migration

---
+++ GoalSetter/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password
+    @password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.update(session_token: self.class.generate_session_token)
+    self.session_token
+  end
+end
