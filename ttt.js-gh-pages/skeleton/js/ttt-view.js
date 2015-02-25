5b1cf520148efb2796a86f640886c28bedc3b57b
Jacob Shorty <jakeshorty@gmail.com>
2015-02-25 12:00:41 -0500

    Finish tic tac toe

--- ttt.js-gh-pages/skeleton/js/ttt-view.js
+++ ttt.js-gh-pages/skeleton/js/ttt-view.js
@@ -6,18 +6,49 @@
   var View = TTT.View = function (game, $el) {
     this.game = game;
     this.$el = $el;
+    this.setupBoard();
   };
 
   View.prototype.bindEvents = function () {
+    var view = this;
+    this.$el.find("li").on("click", function (event) {
+      var $target = $(event.currentTarget);
+      view.makeMove($target);
+    })
   };
 
   View.prototype.makeMove = function ($square) {
+    var val = parseInt($square.attr("pos"));
+    var pos = this.parseVal(val);
+    if (this.game.board.isEmptyPos(pos)) {
+      $square.addClass(this.game.currentPlayer);
+      this.game.board.placeMark(pos, this.game.currentPlayer);
+      this.game.swapTurn();
+      if (this.game.board.isOver() && this.game.board.winner() === null) {
+        alert("Tie game.");
+      }
+      else if (this.game.board.isOver()) {
+        alert(this.game.board.winner() + " wins!");
+      }
+    }
+    else {
+      alert("Invalid move!");
+    }
   };
 
   View.prototype.setupBoard = function () {
     this.$el.append("<ul></ul>");
     var $grid = $("ul");
     $grid.addClass("grid group");
-    $grid.append("<li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>");
+    for (var i = 0; i < 9; i++) {
+      var $li = $("<li></li>");
+      $li.attr("pos", i);
+      $grid.append($li)
+    }
+  };
+
+  View.prototype.parseVal = function(val) {
+    var positions = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
+    return positions[val];
   };
 })();
