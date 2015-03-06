a20b3a2dd9b321852f0fb7b9b2f23f646bbceb47
Jacob Shorty <jakeshorty@gmail.com>
2015-03-06 16:20:59 -0500

    Utilize CompositeView class for Boards

--- app/assets/javascripts/views/board_show.js
+++ app/assets/javascripts/views/board_show.js
@@ -1,29 +1,25 @@
-TrelloClone.Views.BoardShow = Backbone.View.extend({
+TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   template: JST["board_show"],
 
   initialize: function (options) {
     this.model = options.model;
     this.listenTo(this.model, "sync", this.render);
-    this.subviews = [];
   },
 
   render: function () {
     var content = this.template({board: this.model});
     this.$el.html(content);
     this.model.lists().each(function (list) {
-      var subview = new TrelloClone.Views.BoardShowItem({model: list});
-      console.log(this.subviews);
-      this.subviews.push(subview)
-      this.$el.find("ul").append(subview.render().$el);
+      this.addSubview("ul", new TrelloClone.Views.BoardShowItem({model: list}));
     }, this)
     return this;
   },
-
-  remove: function () {
-    Backbone.View.prototype.remove.call(this);
-    this.subviews.forEach(function (subview) {
-      subview.remove();
-    });
-    this.subviews = [];
-  }
+  // 
+  // remove: function () {
+  //   Backbone.View.prototype.remove.call(this);
+  //   this.subviews.forEach(function (subview) {
+  //     subview.remove();
+  //   });
+  //   this.subviews = [];
+  // }
 })
